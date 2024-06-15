3e987ef1b0540e43b35cf1ab231afaa881961401
Agustin <agustinjnadal@gmail.com>
2024-06-14 22:55:58 -0300

    Limpieza

--- app/(app)/_layout.jsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-import React from "react";
-
-const AppLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
-};
-
-export default AppLayout;
